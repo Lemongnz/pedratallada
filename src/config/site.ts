7ef40ad4e50c756113fab69788ff29b6768a9cd9
Nazareno <lemongnz@gmail.com>
2026-03-12 19:21:04 -0300

    feat: add analitycs

---
+++ src/config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  brandName: 'Pedra Tallada',
+  contactEmail: 'arqueopedratallada@gmail.com',
+  whatsappNumber: '34687196748',
+  whatsappDisplayNumber: '+34 687 196 748',
+} as const;
